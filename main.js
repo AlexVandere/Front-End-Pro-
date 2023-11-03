4e810862673a57dc45e15edaa2164a0c0f335a57
alex <koxnoo@gmail.com>
2023-11-03 08:24:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// prompt('what is your name')
+
+// let john = prompt('Hello, John! How are you?')
+// alert(`"Hello, ${john}! How are you?"`)
+
+
+// let x = 3 
+// x=x+5
+
+
+
+let a = Number(prompt())
+let b = Number(prompt())
+let c= Number(prompt())
+alert(a+b+c)
